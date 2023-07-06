e50c9f94477c493e439618514361a82fc8a02959
bhargavacharya123 <bhargavacharya4@gmail.com>
2023-07-06 15:49:37 +0530

    updateEmp method added

--- src/Main.java
+++ src/Main.java
@@ -33,14 +33,43 @@ public class Main {
 		System.out.println("The available employees with us ");
 		for(Emp e1 : al)
 		{
-			System.out.println(e1.getEno()+" "+e1.getEname()+" "+e1.getSal()+" "
-		+e1.getDesg());
+		System.out.println(e1.getEno()+" "+e1.getEname()+" "+
+		e1.getSal()+" "+e1.getDesg());
 		}
-		System.out.println("==========================================");
+System.out.println("==========================================");
 	}
 	void updateEmp()
 	{
+		System.out.println("Enter the emp no to update");
 		
+		int eno = sc.nextInt();
+		
+		System.out.println("Enter the emp name ");
+		String ename = sc.next();
+		
+		System.out.println("Enter the emp sal");
+		double esal = sc.nextDouble();
+		
+		System.out.println("Enter the emp desg");
+		String desg = sc.next();
+		Emp e = new Emp(eno,ename,esal,desg);
+		boolean flag = false;
+		for(Emp e1 : al)
+		{
+			if(e1.getEno() == e.getEno())
+			{
+				e1 = e;
+				System.out.println("employee details modified");
+				flag =true;
+				break;
+			}
+		}
+		if(flag == false)
+		{
+			System.out.println("Emp record not found");
+			
+		}
+		System.out.println("==========================");
 	}
 	void deleteEmp()
 	{
@@ -69,6 +98,7 @@ public class Main {
 	    	    m.viewEmp();
 	    	     break;
 	     case 3:
+	    	    m.updateEmp();
 	    	    break;
 	     case 4:
 	    	 
